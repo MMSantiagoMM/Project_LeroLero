50c06b9d0b0ac23720a02b0179e0471b76c3afa4
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-07-30 19:07:46 -0500

    se agregan los productos a ofrecer y el logo en tamaño mobile

--- index.js
+++ index.js
@@ -4,6 +4,68 @@ let shoppingCar = document.querySelector(".nav-bar-shopping-car")
 let myOrderDisplay = document.querySelector(".myOrder-display")
 let menuMobileIcon = document.querySelector(".menu-mobile-icon")
 let menuMobileDisplay = document.querySelector(".menu-mobile-display")
+let cardsContainer = document.querySelector(".cards-container")
+const productList = [];
+productList.push({
+    name: 'Torta',
+    price: 35.000,
+    img: '/img/cake.jpg'
+})
+productList.push({
+    name: 'Pastel',
+    price: 55.000,
+    img: '/img/cake.jpg'
+})
+productList.push({
+    name: 'Dulces',
+    price: 15.000,
+    img: '/img/cake.jpg'
+})
+productList.push({
+    name: 'Torta',
+    price: 25.000,
+    img: '/img/cake.jpg'
+})
+
+function renderProductos(array){
+    for (product of array) {
+        const productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+
+        const productImg = document.createElement('img')
+        productImg.setAttribute('src',product.img)
+
+        const productInfo = document.createElement('div')
+        productInfo.classList.add('product-info')
+
+        const productInfoDiv = document.createElement('div')
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText = `$ ${product.price}`
+
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+
+        const productInfoFigure = document.createElement('figure')
+        const productImgCard = document.createElement('img')
+        productImgCard.setAttribute('src', './img/shopping-cart.png')
+
+        productInfoFigure.appendChild(productImgCard)
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productInfoFigure)
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+        cardsContainer.appendChild(productCard)
+
+    }
+}
+renderProductos(productList)
+
+
+
+
 
 
 emailDisplay.addEventListener("click",()=>{
