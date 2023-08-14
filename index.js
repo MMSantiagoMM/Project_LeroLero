11fd0b0eafd38da8e712ba2daafea6ee3ce1d2f6
MMSantiagoMM <santiagomosquera2@gmail.com>
2023-08-13 19:57:29 -0500

    avances en el proyecto

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ let aside = document.querySelector('aside')
 let btnCloseAside = document.querySelector('.close-aside')
 let mainContainer = document.querySelector('.main-container')
 let ShoppingCounter = document.querySelector('.shopping-counter')
-let shoppingNumber = 1;
+let MyOrderTotal = document.querySelector('.myOrder-total')
+let TotalPriceMyOrder = document.querySelector('.total-price-myOrder')
+let BtnAside = document.querySelector('.btn-aside')
+let shoppingNumber = 0;
+ShoppingCounter.textContent = shoppingNumber
+let number = 0;
+
+
 
 
 
@@ -25,7 +32,7 @@ productList.push({
     img: '/img/birthday.webp'
 })
 productList.push({
-    name: 'Dulces',
+    name: 'Other sweets',
     price: 15.000,
     img: '/img/best-cookies.webp'
 })
@@ -40,20 +47,20 @@ productList.push({
     img: '/img/rainbow.webp'
 })
 
-function renderProductos(array){
-    array.forEach(product=> {
+function renderProductos(array) {
+    array.forEach(product => {
         const productCard = document.createElement('div')
         productCard.classList.add('product-card')
 
         const productImg = document.createElement('img')
-        productImg.setAttribute('src',product.img)
-        productImg.addEventListener('click',function(){
+        productImg.setAttribute('src', product.img)
+        productImg.addEventListener('click', function () {
             aside.classList.toggle('inactive')
             accountEmail.classList.add('inactive')
             myOrderDisplay.classList.add('inactive')
             renderAside(product)
-
         })
+        
 
         const productInfo = document.createElement('div')
         productInfo.classList.add('product-info')
@@ -61,7 +68,7 @@ function renderProductos(array){
         const productInfoDiv = document.createElement('div')
 
         const productPrice = document.createElement('p')
-        productPrice.innerText = `$ ${product.price}`
+        productPrice.innerText = `$ ${product.price.toFixed(3)}`
 
         const productName = document.createElement('p')
         productName.innerText = product.name
@@ -69,10 +76,50 @@ function renderProductos(array){
         const productInfoFigure = document.createElement('figure')
         const productImgCard = document.createElement('img')
         productImgCard.setAttribute('src', './img/shopping-cart.png')
-        productImgCard.addEventListener('click',()=>{
-            ShoppingCounter.textContent = shoppingNumber++
 
 
+
+        productImgCard.addEventListener('click', () => {
+
+            shoppingNumber++
+            ShoppingCounter.textContent = shoppingNumber
+
+            console.log(shoppingNumber)
+
+
+            let divOrder = document.createElement('div')
+            let imgOrder = document.createElement('img')
+            imgOrder.setAttribute('src', product.img)
+            imgOrder.classList.add('img-myorder')
+            let nameOrder = document.createElement('p')
+            nameOrder.classList.add('parrafo_1')
+            nameOrder.textContent = product.name
+            let priceOrder = document.createElement('p')
+            priceOrder.classList.add('parrafo_2')
+            priceOrder.textContent = '$' + product.price.toFixed(3)
+            let cancelMyOrder = document.createElement('p')
+            number += product.price
+            TotalPriceMyOrder.textContent = '$' + number.toFixed(3)
+            cancelMyOrder.classList.add('parrafo_3')
+            let imgCancelMyOrder = document.createElement('img')
+            imgCancelMyOrder.setAttribute('src', '/img/x.png')
+            imgCancelMyOrder.classList.add('img-x')
+            cancelMyOrder.appendChild(imgCancelMyOrder)
+            divOrder.appendChild(imgOrder)
+            divOrder.appendChild(nameOrder)
+            divOrder.appendChild(priceOrder)
+            divOrder.appendChild(cancelMyOrder)
+            myOrderDisplay.appendChild(divOrder)
+            MyOrderTotal.insertAdjacentElement('beforebegin', divOrder)
+
+            cancelMyOrder.addEventListener('click', () => {
+                divOrder.remove()
+
+                number -= product.price
+                TotalPriceMyOrder.textContent = '$' + number.toFixed(3)
+                ShoppingCounter.textContent = --shoppingNumber
+
+            })
 
         })
 
@@ -85,27 +132,29 @@ function renderProductos(array){
         productCard.appendChild(productInfo)
         cardsContainer.appendChild(productCard)
 
-      
 
-        
+
+
 
     }
-)}
+    )
+}
 
-function renderAside(object){
+function renderAside(object) {
 
     let imgAside = document.querySelector('.aside-right > img')
-    imgAside.setAttribute('src',object.img)
+    imgAside.setAttribute('src', object.img)
 
     let priceAside = document.querySelector('.price-name > h3')
-    priceAside.textContent = '$ '+ object.price
+    priceAside.textContent = '$ ' + object.price.toFixed(3)
 
     let nameAside = document.querySelector('.price-name > p')
     nameAside.textContent = object.name
 
-    btnCloseAside.addEventListener('click',()=>{
+    btnCloseAside.addEventListener('click', () => {
         aside.classList.add('inactive')
     })
+
 }
 
 renderProductos(productList)
@@ -115,45 +164,50 @@ renderProductos(productList)
 
 
 
-emailDisplay.addEventListener("click",()=>{
-    
+
+
+
+
+
+emailDisplay.addEventListener("click", () => {
+
     let isClose = myOrderDisplay.classList.contains("inactive")
-    if(!isClose){
+    if (!isClose) {
         myOrderDisplay.classList.add("inactive")
     }
     accountEmail.classList.toggle("inactive")
     aside.classList.add('inactive')
-    
+
 })
-shoppingCar.addEventListener("click",()=>{
+shoppingCar.addEventListener("click", () => {
     let isClose = accountEmail.classList.contains("inactive")
     let isClosetwo = aside.classList.contains('inactive')
 
-    if(!isClose){
+    if (!isClose) {
         accountEmail.classList.add("inactive")
-    }else{
+    } else {
         myOrderDisplay.classList.toggle("inactive")
     }
-    if(!isClosetwo){
+    if (!isClosetwo) {
         aside.classList.add('inactive')
     }
-}) 
+})
 
-menuMobileIcon.addEventListener("click",()=>{
+menuMobileIcon.addEventListener("click", () => {
     menuMobileDisplay.classList.toggle('inactive')
     let isClose = menuMobileDisplay.classList.contains('inactive')
-    if(isClose){
+    if (isClose) {
         mainContainer.classList.remove('inactive')
-    }else{
+    } else {
         mainContainer.classList.add('inactive')
         myOrderDisplay.classList.add('inactive')
     }
 
-  
+
 })
 
-/* btnCloseAside.addEventListener('click',()=>{
-    aside.classList.add('inactive')
-})  */
+
+
+
 
 
